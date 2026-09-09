6cf3fbb4cefc6e263bc3e2fc471a68b0addd0290
Osama Alkatib <yunisashraf@gmail.com>
2026-09-09 15:36:39 +0300

    Add delete dialog and Context functionality

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,6 +2,12 @@
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
 
 // Icons
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
@@ -10,6 +16,7 @@ import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined
 
 // Hooks
 import { useContext } from 'react';
+import { useState } from 'react';
 
 // Components
 import TodosContext from "../Context/todosContext"
@@ -20,7 +27,29 @@ import "./ToDo.css";
 
 export default function ToDo({todo, handCheck}){
   const {todos, setTodos} = useContext(TodosContext)
+  const [showDeleteDialog, setshowDeleteDialog] = useState(false);
+  
+  // function for Delete Button
+  const handleDeleteOpen = () => {
+    setshowDeleteDialog(true);
+  };
 
+  const handleDeleteClose = () => {
+    setshowDeleteDialog(false);
+  };
+
+  function handleDeleteConfirm(){
+    const updatedTodos = todos.filter( (t) => {
+       if(t.id == todo.id){
+         return false
+       }else{
+         return true 
+       }
+    })
+      setTodos(updatedTodos)
+  }
+
+  // function for Check Button
   function handCheckClick(){
     const updateTodos = todos.map( (t) => {
          if(t.id == todo.id){
@@ -36,7 +65,32 @@ export default function ToDo({todo, handCheck}){
   }
 
     return(
-      <div>  
+      <div>
+          <Dialog          
+              open={showDeleteDialog}
+              onClose={handleDeleteClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+              role="alertdialog"
+              style={{
+                direction:"rtl"
+              }}
+            >
+              <DialogTitle id="alert-dialog-title">
+                {"هل انت متأكد من حذف هذه المهمة؟"}
+              </DialogTitle>
+               <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  لا يمكنك التراجع عن الحذف بعد اتمامه 
+                </DialogContentText>
+               </DialogContent>
+               <DialogActions>
+                <Button onClick={handleDeleteClose} autoFocus >
+                  اغلاق
+                </Button>
+                <Button onClick={handleDeleteConfirm}>تأكيد الحذف</Button>
+               </DialogActions>
+            </Dialog>
        <Card 
          className='todoCard'
          sx={{
@@ -86,7 +140,16 @@ export default function ToDo({todo, handCheck}){
           </IconButton>
           
            {/* DELETE ICON BUTTON */}
-          <IconButton aria-label="delete" size="large" sx={{color:"#EF5350", background:"#fff", border: "2px solid #EF5350", "&:hover": {background: "#FFCDD2"}}}>
+          <IconButton 
+            aria-label="delete"
+            size="large"
+            onClick={handleDeleteOpen}
+            sx={{
+              color:"#EF5350",
+              background:"#fff",
+              border: "2px solid #EF5350",
+              "&:hover": {background: "#FFCDD2"
+            }}}>
             <DeleteOutlineOutlinedIcon />
           </IconButton>
          
